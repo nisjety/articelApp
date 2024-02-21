85bd6dd1c875827392ca36f01338be1d845d19c2
nisjety <ima_stovers24@hotmail.com>
2024-02-21 06:09:51 +0100

    Fixed Heroku build error

--- tests/auth.test.js
+++ tests/auth.test.js
@@ -4,6 +4,8 @@ const express = require('express');
 const session = require('express-session');
 const passport = require('passport');
 const authRouter = require('../routes/auth'); // Adjust the path as needed
+// Laster inn konfigurasjon
+dotenv.config({ path: './config/config.env' })
 
 // Setup express app and middleware for testing
 const app = express();
